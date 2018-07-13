04b4170858115d8b894a8ca858772fedb2fc3c48
Megan Nepshinsky <mnepshinsky@gmail.com>
2018-07-13 19:08:14 +0000

    automatically backed up by learn

--- car.rb
+++ car.rb
@@ -1,21 +1,11 @@
 class Car 
-  attr_reader :model , :make , :year 
+  attr_reader :model , :make , :year ; 
+  attr_accessor :heating 
+  
   def initialize(model, make, year)
     @model = model 
     @make = make 
     @year = year 
-  end 
-  
-  def model 
-    @model 
-  end 
-  
-  def make 
-    @make 
-  end
-  
-  def year 
-    @year
   end 
   
   def heating=(heating)
